326f645d36010602238ac55fcdf4b8689db1bff8
Jeyson Mirabal <jeyson.mirabal@gmail.com>
2024-06-24 16:00:33 -0400

    feat(004): add markers

--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,6 +1,6 @@
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { PlacesService } from '../../services';
-import Map from 'mapbox-gl';
+import {Map, Popup, Marker} from 'mapbox-gl';
 import mapboxgl from 'mapbox-gl';
 
 @Component({
@@ -18,11 +18,25 @@ export class MapViewComponent implements AfterViewInit {
   }
 
   ngAfterViewInit(): void{
+    if (!this.placesService.userLocation) throw Error('Does not exist placesService.userLocation');
+
+
     const map = new mapboxgl.Map({
       container: this.mapDivElement.nativeElement,
       style: 'mapbox://styles/mapbox/light-v10', // style URL
       center: this.placesService.userLocation, // starting position [lng, lat]
       zoom: 14, // starting zoom
     });
+
+    const popup = new Popup()
+    .setHTML(`
+      <h6>Aqui estoy</h6>
+      <span>Estoy en este lugar del mundo</span>
+    `)
+
+    new Marker({ color: 'red'})
+    .setLngLat( this.placesService.userLocation )
+    .setPopup( popup )
+    .addTo( map) 
   }
 }
