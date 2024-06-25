7ae2131a371d4917d2c4705129f9c3e5b8835230
Jeyson Mirabal <jeyson.mirabal@gmail.com>
2024-06-25 16:44:32 -0400

    feat(005): add button and logo

---
+++ src/app/maps/components/btn-my-location/btn-my-location.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-btn-my-location',
+  standalone: true,
+  imports: [],
+  templateUrl: './btn-my-location.component.html',
+  styleUrl: './btn-my-location.component.css'
+})
+export class BtnMyLocationComponent {
+
+  constructor(){}
+
+  goToMyLocation(){
+    console.log('goToMyLocation');
+  }
+}
